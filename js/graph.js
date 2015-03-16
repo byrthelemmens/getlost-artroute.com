2a1ce58329e0b6137e63dd9f11cc43d40af00bde
Gilles Wittenberg <git@gilleswittenberg.com>
2015-03-16 15:18:48 +0100

    Hide and show background

--- js/graph.js
+++ js/graph.js
@@ -1,25 +1,48 @@
 
 // elements
-var graphWrapperElement = document.getElementById('graph-wrapper');
+var graphBackgroundElement = document.getElementById('graph-background');
 var graphElement = document.getElementById('graph');
-var canvasWidth = graphElement.width = graphWrapperElement.offsetWidth;
-var canvasHeight = graphWrapperElement.offsetHeight;
+var canvasWidth, canvasHeight, scale;
 
 // draw
 var context = graphElement.getContext('2d');
 
 // data
 var minValue = 350, maxValue = 550;
-var scale = canvasHeight / (maxValue - minValue);
 
-$.getJSON('data/aex.json', function (data) {
-	draw(data);
+setCanvasDimensions();
+
+var url = 'data/aex.json'; 
+var values;
+
+$.getJSON(url, function (data) {
+	values = data;
+	draw(values);
+	showBackground();
 });
+
+window.addEventListener('resize', setCanvasDimensions);
+
+function setCanvasDimensions () {
+	var height = Math.round((graphBackgroundElement.offsetWidth / 1920) * 550) + 'px';
+	graphBackgroundElement.setAttribute('style', 'height: ' + height);
+	canvasWidth = graphElement.width = graphBackgroundElement.offsetWidth;
+	canvasHeight = graphElement.height = graphBackgroundElement.offsetHeight;
+	scale = canvasHeight / (maxValue - minValue);
+	if (values) {
+		draw(values);
+	}
+}
+
+function showBackground () {
+	graphBackgroundElement.classList.remove('background-hidden');
+}
 
 function draw (data) {
 	var length = data.aex.length;
 	var width = Math.ceil(canvasWidth / length);
 	var x = 0;
+	// draw 
 	data.aex.forEach(function (row) {
 		drawBar(x, getY(row.value), width, getHeight(row.value));
 		x += width;
