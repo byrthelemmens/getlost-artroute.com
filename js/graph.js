b1566bc38dc575591aa02a6db85c0c55d66370bc
Gilles Wittenberg <git@gilleswittenberg.com>
2015-04-14 18:30:10 +0200

    Fix scaling graph

--- js/graph.js
+++ js/graph.js
@@ -1,123 +1,124 @@
+(function () {
 
-// elements
-var graphWrapperElement = document.getElementById('graph-wrapper');
-var graphElement = document.getElementById('graph');
-var wrapElement = document.getElementById('wrap');
-var programElement = document.getElementById('program');
-var canvasWidth, canvasHeight, scale;
+	// elements
+	var graphWrapperElement = document.getElementById('graph-wrapper');
+	var graphElement = document.getElementById('graph');
+	var wrapElement = document.getElementById('wrap');
+	var programElement = document.getElementById('program');
+	var canvasWidth, canvasHeight, scale;
 
-// draw
-var context = graphElement.getContext('2d');
+	// draw
+	var context = graphElement.getContext('2d');
 
-// data
-var minValue = 350, maxValue = 900;
+	// data
+	var minValue = 350, maxValue = 900;
 
-setCanvasDimensions();
+	setCanvasDimensions();
 
-var url = 'data/aex_generated.json'; 
-var values;
+	var url = 'data/aex_generated.json'; 
+	var values;
 
-$.getJSON(url, function (data) {
-	values = data;
-	draw(values);
-});
-
-window.addEventListener('resize', resizeHandler);
-
-
-var timeout;
-function resizeHandler (event) {
-	if (timeout) {
-		window.clearTimeout(timeout);
-	}
-	timeout = window.setTimeout(function () {
-		setCanvasDimensions();
-	}, 500);
-}
-
-function setCanvasDimensions () {
-	var height = Math.round((graphWrapperElement.offsetWidth / 1920) * 550) + 'px';
-	graphWrapperElement.setAttribute('style', 'height: ' + height + ';');
-	canvasWidth = graphElement.width = graphWrapperElement.offsetWidth;
-	canvasHeight = graphElement.height = graphWrapperElement.offsetHeight;
-	setWrapPaddingTop();
-	scale = canvasHeight / (maxValue - minValue);
-	if (values) {
+	$.getJSON(url, function (data) {
+		values = data;
 		draw(values);
-	}
-}
-
-function setWrapPaddingTop() {
-	var styleAttribute = graphWrapperElement.getAttribute('style') + ' position:fixed;';
-	graphWrapperElement.setAttribute('style', styleAttribute);
-	if (programElement) {
-		programElement.style.height = canvasHeight + 'px';
-	} else {
-		wrapElement.setAttribute('style', 'padding-top: ' + canvasHeight + 'px;');
-	}
-}
-
-function draw (values) {
-
-
-	// graph
-	context.save();
-	context.beginPath();
-	context.moveTo(0, 0);
-	drawLines(values);
-	context.lineTo(canvasWidth, 0);
-	context.closePath();
-	context.fill();
-	context.clip();
-
-	// image
-	var img = new Image();
-	img.onload = function () {
-	    context.drawImage(img, 0, 0, canvasWidth, canvasHeight);
-
-		// text AEX value
-		context.font = '14px Helvetica';
-		context.fillText('AEX', 15, 30);
-		context.font = 'bold 18px Helvetica';
-		var fillText;
-		values.aex.forEach(function (day) {
-			if (day.value) {
-				fillText = day.value;
-			}
-		});
-		context.fillText(parseFloat(fillText, 10).toFixed(2), 15, 50);
-		context.restore();
-	}
-	img.src = '../img/img_graph04.png';
-}
-
-function drawLines (data) {
-
-	var length = data.aex.length;
-	var width = Math.ceil(canvasWidth / length);
-	var x = 0;
-	
-	// draw 
-	data.aex.forEach(function (row) {
-		var value = row.value;
-		var height;
-		if (new Date(row.date) > new Date('06-22-2015')) {
-			value = undefined;
-		}
-		if (value) {
-			drawLine(x, getHeight(value));
-		} else {
-			drawLine(x - width, getHeight(maxValue));
-			drawLine(x, getHeight(maxValue));
-		}
-		x += width;
 	});
-}
 
-function drawLine (x, height) {
-	context.lineTo(x, height);
-}
+	window.addEventListener('resize', resizeHandler);
 
-function getHeight (value) {
-	return (maxValue - value) * scale;
-}
+	var timeout;
+	function resizeHandler (event) {
+		if (timeout) {
+			window.clearTimeout(timeout);
+		}
+		timeout = window.setTimeout(function () {
+			setCanvasDimensions();
+		}, 200);
+	}
+
+	function setCanvasDimensions () {
+		var height = Math.round((graphWrapperElement.offsetWidth / 1920) * 550) + 'px';
+		graphWrapperElement.setAttribute('style', 'height: ' + height + ';');
+		canvasWidth = graphElement.width = graphWrapperElement.offsetWidth;
+		canvasHeight = graphElement.height = graphWrapperElement.offsetHeight;
+		setWrapPaddingTop();
+		scale = canvasHeight / (maxValue - minValue);
+		if (values) {
+			draw(values);
+		}
+	}
+
+	function setWrapPaddingTop() {
+		var styleAttribute = graphWrapperElement.getAttribute('style') + ' position:fixed;';
+		graphWrapperElement.setAttribute('style', styleAttribute);
+		if (programElement) {
+			programElement.style.height = canvasHeight + 'px';
+		} else {
+			wrapElement.setAttribute('style', 'padding-top: ' + canvasHeight + 'px;');
+		}
+	}
+
+	function draw (values) {
+
+
+		// graph
+		context.save();
+		context.beginPath();
+		context.moveTo(0, 0);
+		drawLines(values);
+		context.lineTo(canvasWidth, 0);
+		context.closePath();
+		context.fill();
+		context.clip();
+
+		// image
+		var img = new Image();
+		img.onload = function () {
+		    context.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+
+			// text AEX value
+			context.font = '14px Helvetica';
+			context.fillText('AEX', 15, 30);
+			context.font = 'bold 18px Helvetica';
+			var fillText;
+			values.aex.forEach(function (day) {
+				if (day.value) {
+					fillText = day.value;
+				}
+			});
+			context.fillText(parseFloat(fillText, 10).toFixed(2), 15, 50);
+			context.restore();
+		}
+		img.src = '../img/img_graph04.png';
+	}
+
+	function drawLines (data) {
+
+		var length = data.aex.length;
+		var width = Math.ceil(canvasWidth / length);
+		var x = 0;
+		
+		// draw 
+		data.aex.forEach(function (row) {
+			var value = row.value;
+			var height;
+			if (new Date(row.date) > new Date('06-22-2015')) {
+				value = undefined;
+			}
+			if (value) {
+				drawLine(x, getHeight(value));
+			} else {
+				drawLine(x - width, getHeight(maxValue));
+				drawLine(x, getHeight(maxValue));
+			}
+			x += width;
+		});
+	}
+
+	function drawLine (x, height) {
+		context.lineTo(x, height);
+	}
+
+	function getHeight (value) {
+		return (maxValue - value) * scale;
+	}
+}());
--- js/program.js
+++ js/program.js
@@ -1,85 +1,86 @@
+(function () {
+	var programElement = document.getElementById('program');
+	var monthsElement = document.getElementById('program-months');
+	var eventDataElement = document.getElementById('program-event-data');
 
-var programElement = document.getElementById('program');
-var monthsElement = document.getElementById('program-months');
-var eventDataElement = document.getElementById('program-event-data');
-
-var url = 'data/aex_generated.json'; 
-var json;
-$.getJSON(url, function (data) {
-	json = data;
-	initProgram();
-});
-
-window.addEventListener('resize', resizeHandler);
-
-var timeout;
-function resizeHandler (event) {
-	if (timeout) {
-		window.clearTimeout(timeout);
-	}
-	timeout = window.setTimeout(function () {
-		resetEvents();
-	}, 500);
-}
-
-function resetEvents () {
-
-	var elements = monthsElement.querySelectorAll('.program-event');
-	Array.prototype.forEach.call(elements, function (elem) {
-		monthsElement.removeChild(elem);
+	var url = 'data/aex_generated.json'; 
+	var json;
+	$.getJSON(url, function (data) {
+		json = data;
+		initProgram();
 	});
-	json.aex.forEach(addDayEvents);
-}
 
-function initProgram () {
+	window.addEventListener('resize', resizeHandler);
 
-	json.aex.forEach(addDayBar);
-	json.aex.forEach(addDayEvents);
-}
-
-function addDayBar (day) {
-
-	// bar
-	var div = document.createElement('div');
-	div.classList.add('program-bar');
-	var percentage = 100 / json.aex.length;
-	div.style.width = percentage + '%';
-	programElement.insertBefore(div, programElement.firstChild);
-}
-
-function addDayEvents (day, index) {
-
-	// event
-	if (typeof day.events !== 'undefined' && day.events.length) {
-		day.events.forEach(function (event) {
-			var elem = document.createElement('span');
-			elem.classList.add('program-event');
-			elem.style.left = (index * (programElement.offsetWidth / json.aex.length)) + 'px';
-			elem.setAttribute('data-event-date', day.date);
-			elem.setAttribute('data-event-title', event.title);
-			elem.setAttribute('data-event-description', event.description);
-			elem.addEventListener('mouseover', mouseoverHandler);
-			monthsElement.appendChild(elem);
-		});
+	var timeout;
+	function resizeHandler (event) {
+		if (timeout) {
+			window.clearTimeout(timeout);
+		}
+		timeout = window.setTimeout(function () {
+			resetEvents();
+		}, 500);
 	}
-}
 
-function mouseoverHandler (event) {
+	function resetEvents () {
 
-	var date = getDisplayDate(event.currentTarget.getAttribute('data-event-date'));
-	var title = event.currentTarget.getAttribute('data-event-title');
-	var description = event.currentTarget.getAttribute('data-event-description');
-	eventDataElement.innerHTML = '<h1>' + date + ': ' + title + '</h1><p>' + description + '</p>';
+		var elements = monthsElement.querySelectorAll('.program-event');
+		Array.prototype.forEach.call(elements, function (elem) {
+			monthsElement.removeChild(elem);
+		});
+		json.aex.forEach(addDayEvents);
+	}
 
-	var left = parseInt(event.currentTarget.style.left, 10)
-	var programElementWidth = programElement.offsetWidth;
-	var eventDataElementWidth = eventDataElement.offsetWidth;
-	left = left > programElementWidth - eventDataElementWidth ? programElementWidth - eventDataElementWidth : left;
-	eventDataElement.style.left = left + 'px';
-}
+	function initProgram () {
 
-function getDisplayDate (date) {
-	var date = new Date(date);
-	var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	return date.getDate() + ' ' + monthNames[date.getMonth()];
-}
+		json.aex.forEach(addDayBar);
+		json.aex.forEach(addDayEvents);
+	}
+
+	function addDayBar (day) {
+
+		// bar
+		var div = document.createElement('div');
+		div.classList.add('program-bar');
+		var percentage = 100 / json.aex.length;
+		div.style.width = percentage + '%';
+		programElement.insertBefore(div, programElement.firstChild);
+	}
+
+	function addDayEvents (day, index) {
+
+		// event
+		if (typeof day.events !== 'undefined' && day.events.length) {
+			day.events.forEach(function (event) {
+				var elem = document.createElement('span');
+				elem.classList.add('program-event');
+				elem.style.left = (index * (programElement.offsetWidth / json.aex.length)) + 'px';
+				elem.setAttribute('data-event-date', day.date);
+				elem.setAttribute('data-event-title', event.title);
+				elem.setAttribute('data-event-description', event.description);
+				elem.addEventListener('mouseover', mouseoverHandler);
+				monthsElement.appendChild(elem);
+			});
+		}
+	}
+
+	function mouseoverHandler (event) {
+
+		var date = getDisplayDate(event.currentTarget.getAttribute('data-event-date'));
+		var title = event.currentTarget.getAttribute('data-event-title');
+		var description = event.currentTarget.getAttribute('data-event-description');
+		eventDataElement.innerHTML = '<h1>' + date + ': ' + title + '</h1><p>' + description + '</p>';
+
+		var left = parseInt(event.currentTarget.style.left, 10)
+		var programElementWidth = programElement.offsetWidth;
+		var eventDataElementWidth = eventDataElement.offsetWidth;
+		left = left > programElementWidth - eventDataElementWidth ? programElementWidth - eventDataElementWidth : left;
+		eventDataElement.style.left = left + 'px';
+	}
+
+	function getDisplayDate (date) {
+		var date = new Date(date);
+		var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		return date.getDate() + ' ' + monthNames[date.getMonth()];
+	}
+}());
